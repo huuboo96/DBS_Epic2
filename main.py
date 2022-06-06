89055370a43bdf05e08386bedae21cdb073c6345
huuboo96 <hubertkozuchowski@gmail.com>
2022-06-07 00:21:33 +0200

    Update main.py
    Updated Main.py python function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+
+import boto3
 import json
 
 def lambda_handler(event, context):
@@ -6,3 +9,28 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Test')
     }
+
+print('Loading function')
+
+
+def handler(event, context):
+
+    operation = event['operation']
+
+    if 'tableName' in event:
+        dynamo = boto3.resource('dynamodb').Table(event['tableName'])
+
+    operations = {
+        'create': lambda x: dynamo.put_item(**x),
+        'read': lambda x: dynamo.get_item(**x),
+        'update': lambda x: dynamo.update_item(**x),
+        'delete': lambda x: dynamo.delete_item(**x),
+        'list': lambda x: dynamo.scan(**x),
+        'echo': lambda x: x,
+        'ping': lambda x: 'pong'
+    }
+
+    if operation in operations:
+        return operations[operation](event.get('payload'))
+    else:
+        raise ValueError('Unrecognized operation "{}"'.format(operation))
